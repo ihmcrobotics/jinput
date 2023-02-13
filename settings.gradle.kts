11c6d967bd1c9f7a421bae13766659bed1bef52e
Duncan Calvert <dcalvert@ihmc.us>
2023-02-13 11:47:33 -0600

    :arrow_up: ihmc-build 0.28.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
       gradlePluginPortal()
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.28.6"
+      id("us.ihmc.ihmc-build") version "0.28.7"
    }
 }
 
@@ -14,7 +14,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.28.6")
+      classpath("us.ihmc:ihmc-build:0.28.7")
    }
 }
 
