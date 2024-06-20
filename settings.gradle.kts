ccda77f9f471748e45c122ca3fa14cf32a166bb1
Dexton Anderson <danderson@ihmc.org>
2024-06-20 09:52:21 -0500

    :arrow_up: ihmc-build 0.29.6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
       gradlePluginPortal()
    }
    plugins {
-      id("us.ihmc.ihmc-build") version "0.29.4"
+      id("us.ihmc.ihmc-build") version "0.29.6"
    }
 }
 
@@ -14,7 +14,7 @@ buildscript {
       mavenLocal()
    }
    dependencies {
-      classpath("us.ihmc:ihmc-build:0.29.4")
+      classpath("us.ihmc:ihmc-build:0.29.6")
    }
 }
 
